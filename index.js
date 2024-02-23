8258d55bdfae4755ba65e0ab53be20f888327f71
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-02-23 03:34:30 +0300

    add changes and local storge

--- index.js
+++ index.js
@@ -1,43 +1,116 @@
-// Variables
+let inputTask = document.querySelector('#inputTask');
+let addBtn = document.querySelector('#add-task');
+let tasksList = document.querySelector('#tasksList');
 
-const addTask = document.getElementById('add-task');
-const taskContainer = document.getElementById('task-container');
-const inputTask = document.getElementById('input-task');
+// Empty Array To Store The Tasks
+let arrayOfTasks = [];
 
-// Event Listener for Add Button
-let task = document.createElement('ul');
-task.classList.add('Task');
+// Check if Theres Tasks In Local Storage
+if (localStorage.getItem('tasks')) {
+  arrayOfTasks = JSON.parse(localStorage.getItem('tasks'));
+}
 
-addTask.addEventListener('click', function () {
+// Trigger Get Data From Local Storage Function
+getDataFromLocalStorage();
 
-  let li = document.createElement('li');
-  li.innerText = `${inputTask.value}`;
-  task.appendChild(li);
-
-  let checkButton = document.createElement('button');
-  checkButton.innerHTML = `<i class="fa-solid fa-check"></i>`;
-  checkButton.classList.add('checkTask');
-  task.appendChild(checkButton);
-
-  let deleteButton = document.createElement('button');
-  deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-  deleteButton.classList.add('deleteTask');
-  task.appendChild(deleteButton);
-
-  if (inputTask.value === '') {
-    alert('Please Enter a Task');
+// Add Task
+addBtn.onclick = function () {
+  if (inputTask.value !== '') {
+    addTaskToArray(inputTask.value); // Add Task To Array Of Tasks
+    inputTask.value = ''; // Empty inputTask Field
   } else {
-    taskContainer.appendChild(task);
+    alert('Please Enter a Task');
   }
+};
 
-  inputTask.value = '';
+function addTaskToArray(taskText) {
+  // Task Data
+  const task = {
+    id: Date.now(),
+    title: taskText,
+    completed: false,
+  };
+  // Push Task To Array Of Tasks
+  arrayOfTasks.push(task);
+  // Add Tasks To Page
+  addElementsToPageFrom(arrayOfTasks);
+  // Add Tasks To Local Storage
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
 
-  checkButton.addEventListener('click', function () {
-    checkButton.parentElement.style.textDecoration = 'line-through';
+function addElementsToPageFrom(arrayOfTasks) {
+  // Empty Tasks taskLi
+  tasksList.innerHTML = '';
+  // Looping On Array Of Tasks
+  arrayOfTasks.forEach((task) => {
+    // Create Main taskLi
+    let taskLi = document.createElement('li');
+    taskLi.className = 'task';
+
+    // CheckBox
+    let checkButton = document.createElement('input');
+    checkButton.type = `checkbox`;
+    checkButton.classList.add('checkTask');
+    taskLi.appendChild(checkButton);
+
+    // Check If Task is Done
+    if (task.completed) {
+      taskLi.className = 'task done';
+      checkButton.checked = true;
+    }
+
+    let taskTitle = document.createElement('span');
+    taskTitle.textContent = task.title;
+    taskLi.setAttribute('data-id', task.id);
+    taskLi.appendChild(taskTitle);
+    // Create Delete Button
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = `<i class="glyphicon glyphicon-remove"></i>`;
+    deleteButton.classList.add('deleteTask');
+
+    checkButton.addEventListener('click', function (e) {
+      toggleStatusTaskWith(e.target.parentElement.getAttribute('data-id'));
+      // Toggle Done Class
+      e.target.parentElement.classList.toggle('done');
+    });
+
+    deleteButton.addEventListener('click', function (e) {
+      let target = e.target.parentElement.parentElement;
+      target.remove();
+      deleteTaskWith(target.getAttribute('data-id'));
+    });
+
+    // Append Button To Main taskLi
+    taskLi.appendChild(deleteButton);
+    // Add Task taskLi To Tasks Container
+    tasksList.appendChild(taskLi);
   });
+}
 
-  deleteButton.addEventListener('click', function (e) {
-    let target = e.target;
-    target.parentElement.parentElement.remove();
-  });
-});
+function addDataToLocalStorageFrom(arrayOfTasks) {
+  window.localStorage.setItem('tasks', JSON.stringify(arrayOfTasks));
+}
+
+function getDataFromLocalStorage() {
+  let data = window.localStorage.getItem('tasks');
+  if (data) {
+    let tasks = JSON.parse(data);
+    addElementsToPageFrom(tasks);
+  }
+}
+
+function deleteTaskWith(taskId) {
+  arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+function toggleStatusTaskWith(taskId) {
+  for (let i = 0; i < arrayOfTasks.length; i++) {
+    if (arrayOfTasks[i].id == taskId) {
+      arrayOfTasks[i].completed == false
+        ? (arrayOfTasks[i].completed = true)
+        : (arrayOfTasks[i].completed = false);
+    }
+  }
+  addDataToLocalStorageFrom(arrayOfTasks);
+}
