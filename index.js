537ca6bd1ef0f4d486b72a1c72f93999cc0be3ef
Shama Alkhaldi <Shama@alMacBook-Air-alkhas-bbdalzyz.local>
2024-02-26 02:26:32 +0300

    apply feedback, add task description, counter task and delete all button

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let inputTask = document.querySelector('#inputTask');
+let inputTaskDec = document.querySelector('#inputTaskDec');
 let addBtn = document.querySelector('#add-task');
+let deleteAllBtn = document.querySelector('#deleteAll-task');
 let tasksList = document.querySelector('#tasksList');
 let taskSearch = document.querySelector('#taskSearch');
 
@@ -11,24 +13,75 @@ if (localStorage.getItem('tasks')) {
   arrayOfTasks = JSON.parse(localStorage.getItem('tasks'));
 }
 
+// Define progressMessages as an empty array instead of null
+let progressMessages = [];
+
+async function loadProgressMessages() {
+  try {
+    const response = await fetch('progress_messages.json');
+    const data = await response.json();
+    progressMessages = data;
+    // Once progressMessages is loaded, call displayProgressMessage
+    displayProgressMessage();
+  } catch (error) {
+    console.error('Error loading messages:', error);
+  }
+}
+
+// completedTasks / totalTasks
+function displayProgressMessage() {
+  const totalTasks = arrayOfTasks.filter((task) => task).length;
+  const completedTasks = arrayOfTasks.filter((task) => task.completed).length;
+  const progressPercentage = (completedTasks / totalTasks) * 100 || 0;
+  const progressMessage = getProgressMessage(progressPercentage);
+
+  const messageContainer = document.getElementById('quote');
+  messageContainer.textContent = progressMessage;
+}
+
+// get progress messages --------------------
+function getProgressMessage(percentage) {
+  if (!progressMessages) return 'Step by step'; // if messages not loaded yet
+
+  for (const messageData of progressMessages) {
+    if (percentage >= messageData.min && percentage <= messageData.max) {
+      return messageData.message;
+    }
+  }
+  return 'Step by step';
+}
+
 // Trigger Get Data From Local Storage Function
 getDataFromLocalStorage();
 
 // Add Task
 addBtn.onclick = function () {
-  if (inputTask.value !== '') {
-    addTaskToArray(inputTask.value); // Add Task To Array Of Tasks
-    inputTask.value = ''; // Empty inputTask Field
+  if (inputTask.value) {
+    console.log(inputTask.value.length);
+    if (inputTask.value.length <= 30) {
+      addTaskToArray(inputTask.value, inputTaskDec.value); // Add Task To Array Of Tasks
+      inputTask.value = ''; // Empty inputTask Field
+      inputTaskDec.value = '';
+    } else {
+      alert('It is ToDo List NOT Journal 😒');
+    }
   } else {
     alert('Please Enter a Task');
   }
 };
 
-function addTaskToArray(taskText) {
+deleteAllBtn.onclick = function () {
+  arrayOfTasks = [];
+  addDataToLocalStorageFrom(arrayOfTasks);
+  renderTasks(arrayOfTasks);
+};
+
+function addTaskToArray(taskTitle, taskDec) {
   // Task Data
   const task = {
     id: Date.now(),
-    title: taskText,
+    title: taskTitle,
+    description: taskDec,
     completed: false,
   };
 
@@ -36,17 +89,29 @@ function addTaskToArray(taskText) {
   arrayOfTasks.push(task);
 
   // Add Tasks To Page
-  addElementsToPageFrom(arrayOfTasks);
+  renderTasks(arrayOfTasks);
   // Add Tasks To Local Storage
   addDataToLocalStorageFrom(arrayOfTasks);
 }
 
-function addElementsToPageFrom(arrayOfTasks) {
+function renderTasks(arrayOfTasks) {
   // Empty Tasks taskLi
   tasksList.innerHTML = '';
-  console.log(arrayOfTasks);
+  calculateProgress();
+  loadProgressMessages();
+
+  if (arrayOfTasks.length === 0) {
+    // Create a paragraph tag
+    const noTaskMessage = document.createElement('p');
+    noTaskMessage.textContent =
+      "No tasks? Let's change that! 🚀 Add a new task above and get things done!";
+    // Append the paragraph tag to the tasksList container
+    tasksList.appendChild(noTaskMessage);
+    // Exit the function early since there are no tasks to render
+    return;
+  }
   arrayOfTasks.sort((a, b) => (a.completed ? 1 : 0) - (b.completed ? 1 : 0));
-  console.log(arrayOfTasks);
+
   // Looping On Array Of Tasks
   arrayOfTasks.forEach((task) => {
     // Create Main taskLi
@@ -57,7 +122,7 @@ function addElementsToPageFrom(arrayOfTasks) {
     let colorDiv = document.createElement('div');
     colorDiv.className = 'colorDiv';
     taskLi.appendChild(colorDiv);
-    
+
     // Create CheckBox
     let checkButton = document.createElement('input');
     checkButton.type = `checkbox`;
@@ -73,12 +138,23 @@ function addElementsToPageFrom(arrayOfTasks) {
     taskLi.setAttribute('data-id', task.id);
 
     // Create Task
-    let taskTitle = document.createElement('input');
-    taskTitle.type = 'text';
-    taskTitle.value = task.title;
+    let taskTitle = document.createElement('span');
+    taskTitle.textContent = task.title;
     taskTitle.classList.add('taskTitle');
-    taskTitle.setAttribute('readonly', 'readonly');
-    taskLi.appendChild(taskTitle);
+    taskTitle.setAttribute('contenteditable', 'false');
+
+    let taskDec = document.createElement('span');
+    taskDec.textContent = task.description;
+    taskDec.classList.add('taskDec');
+    taskDec.setAttribute('contenteditable', 'false');
+
+    let divTask = document.createElement('div');
+    divTask.classList.add('divTask');
+
+    divTask.appendChild(taskTitle);
+    divTask.appendChild(taskDec);
+
+    taskLi.appendChild(divTask);
 
     // Create Container button
     let taskBtns = document.createElement('div');
@@ -108,13 +184,10 @@ function addElementsToPageFrom(arrayOfTasks) {
       const searchText = e.target.value.toLowerCase();
 
       if (searchText !== '') {
-        for (let index = 0; index < arrayOfTasks.length; index++) {
-          if (task.title.includes(searchText)) {
-            taskLi.style.display = 'flex';
-          } else {
-            taskLi.style.display = 'none';
-          }
-        }
+        let filterArray = arrayOfTasks.filter((task) => {
+          return task.title.includes(searchText);
+        });
+        renderTasks(filterArray);
       } else {
         getDataFromLocalStorage();
       }
@@ -131,14 +204,20 @@ function addElementsToPageFrom(arrayOfTasks) {
       if (
         editButton.innerHTML == `<i class="fa-regular fa-pen-to-square"></i>`
       ) {
-        // editButton.innerHTML = ""
         editButton.innerHTML = `<i class="fa-regular fa-square-check"></i>`;
-        taskTitle.removeAttribute('readonly');
+        taskTitle.contentEditable = true;
+        taskDec.contentEditable = true;
         taskTitle.focus();
       } else {
         editButton.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>`;
-        taskTitle.setAttribute('readonly', 'readonly');
-        editTaskTitle(target.getAttribute('data-id'), taskTitle.value);
+        // taskTitle.setAttribute('readonly', 'readonly');
+        taskTitle.contentEditable = false;
+        taskDec.contentEditable = false;
+        editTaskTitle(
+          target.getAttribute('data-id'),
+          taskTitle.textContent,
+          taskDec.textContent
+        );
       }
     });
 
@@ -158,12 +237,13 @@ function getDataFromLocalStorage() {
   let data = window.localStorage.getItem('tasks');
   if (data) {
     let tasks = JSON.parse(data);
-    addElementsToPageFrom(tasks);
+    renderTasks(tasks);
   }
 }
 
 function deleteTaskWith(taskId) {
   arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
+  calculateProgress();
   addDataToLocalStorageFrom(arrayOfTasks);
 }
 
@@ -177,14 +257,33 @@ function toggleStatusTaskWith(taskId) {
   }
   arrayOfTasks.sort((a, b) => (a.completed ? 1 : 0) - (b.completed ? 1 : 0));
   addDataToLocalStorageFrom(arrayOfTasks);
-  addElementsToPageFrom(arrayOfTasks);
+  renderTasks(arrayOfTasks);
 }
 
-function editTaskTitle(taskId, taskTitle) {
+function editTaskTitle(taskId, taskTitle, taskDec) {
   for (let i = 0; i < arrayOfTasks.length; i++) {
     if (arrayOfTasks[i].id == taskId) {
       arrayOfTasks[i].title = taskTitle;
+      arrayOfTasks[i].description = taskDec;
     }
   }
   addDataToLocalStorageFrom(arrayOfTasks);
+}
+
+// toggle task status ------------------------------------------
+function calculateProgress() {
+  const totalTasks = arrayOfTasks.filter((task) => task).length; // exclude deleted tasks
+  const completedTasks = arrayOfTasks.filter((task) => task.completed).length; // only not-deleted, completed tasks
+  // const deletedTasks = tasks.filter((task) => task.isRemoved).length;
+
+  const progressPercentage = (completedTasks / totalTasks) * 100 || 0;
+
+  // update progress bar
+  const progressBar = document.getElementById('progressBar');
+  progressBar.style.width = `${progressPercentage}%`;
+  console.log(progressPercentage);
+
+  // update progress text
+  const progressText = document.getElementById('progressText');
+  progressText.textContent = `${completedTasks} / ${totalTasks} Completed`; // exclude deleted from total
 }
